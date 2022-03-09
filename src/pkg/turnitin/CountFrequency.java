880a4802572e35ce5a27e2ca66b12d96dc1cf647
gopalgupta8585 <gopalgupta8585@gmail.com>
2022-03-09 12:21:05 -0700

    Initial Submission

---
+++ src/pkg/turnitin/CountFrequency.java
@@ -0,0 +1,34 @@
+package pkg.turnitin;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class CountFrequency {
+
+	public static void main(String[] args) {
+		
+		String st = "the cat is in the bag";
+        String [] arrOfStrings = st.split(" ");
+        Map<String,Integer> map = new HashMap<>();
+        
+        PriorityQueue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<arrOfStrings.length;i++){
+            
+            if(map.containsKey(arrOfStrings[i])){
+                map.put(arrOfStrings[i], map.get(arrOfStrings[i])+1);
+                queue.remove(map.get(arrOfStrings[i])+" "+arrOfStrings[i]);
+                queue.add(map.get(arrOfStrings[i])+" "+arrOfStrings[i]);
+            }else{
+                map.put(arrOfStrings[i], 1);
+                queue.add(map.get(arrOfStrings[i])+" "+arrOfStrings[i]);
+            }
+        }
+        
+        Iterator<String> iterator = queue.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next() + " ");
+        }
+    }
+}
